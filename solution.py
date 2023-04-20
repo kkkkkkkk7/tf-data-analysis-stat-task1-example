dfd4841b330783dcc6562578f8ccdbe990219793
kkkkkkkk7 <128353867+kkkkkkkk7@users.noreply.github.com>
2023-04-20 14:12:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 750949272 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n = 100
+    path = np.random.laplace(loc=100, scale=10, size=n)
+    mean_path = np.mean(path)
+    x = 2 * mean_path / (93 ** 2)
     return x.mean() # Ваш ответ
